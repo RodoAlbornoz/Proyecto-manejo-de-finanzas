e8f5b40e4dc42fa3f1e46434338aadd148e62595
RodoAlbornoz <ralbornozt@fi.uba.ar>
2025-07-13 21:20:29 -0300

    Correccion en nombre de variable de error

--- static/js/validarPresupuesto.js
+++ static/js/validarPresupuesto.js
@@ -3,14 +3,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector("form");
     const nombrePresupuesto = document.getElementById("presupuesto");
-    const errorNombrePresupuesto = document.querySelector("span.error-presupuesto");
+    const errorPresupuesto = document.querySelector("span.error-presupuesto");
 
     form.addEventListener("submit", (e) => {
         if (!nombrePresupuesto.value) {
-            errorNombrePresupuesto.innerHTML = "El nombre del presupuesto es requerido";
+            errorPresupuesto.innerHTML = "El nombre del presupuesto es requerido";
             e.preventDefault();
         } else if (nombrePresupuesto.value.length <= 3) {
-            errorNombrePresupuesto.innerHTML = "El nombre del presupuesto es muy corto";
+            errorPresupuesto.innerHTML = "El nombre del presupuesto es muy corto";
             e.preventDefault();
         }
     });
